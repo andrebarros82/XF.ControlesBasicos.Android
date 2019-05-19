0007b3427f4e921b9239efdef43f40c13b1c2f21
Andre Luis Barros <andrebarros82@gmail.com>
2019-05-19 19:11:15 -0300

    Adicionar arquivos de projeto.

---
+++ XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XF.ControlesBasicos.Views;
+
+namespace XF.ControlesBasicos
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonEnviar_Clicked(object sender, EventArgs e)
+        {
+            string msg = App.ReceberEmail ? $"E-mail enviado para {App.Email}" : "E-mail não autorizado";
+            DisplayAlert("Prática 1", msg, "OK");
+        }
+
+        private void ButtonConfiguracao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConfigPage());
+        }
+    }
+}
---
+++ XF.ControlesBasicos/XF.ControlesBasicos/Views/ConfigPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.ControlesBasicos.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfigPage : ContentPage
+    {
+        public ConfigPage()
+        {
+            InitializeComponent();
+
+            App.ReceberEmail = SwitchEmail.IsToggled;
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            EntryEmail.IsVisible = SwitchEmail.IsToggled;
+            if (!SwitchEmail.IsToggled) EntryEmail.Text = string.Empty;
+            App.ReceberEmail = SwitchEmail.IsToggled;
+        }
+
+        private void EntryEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            App.Email = e.NewTextValue;
+        }
+    }
+}
